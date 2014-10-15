04626ef676f18fea0e007cca386f93a791e5286a
Martial Suchecki <martial.suchecki@gmail.com>
2014-10-15 22:58:56 +0200

    Modif de la méthode main d'HelloWord.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World Java");
+		
+		Eleve e1 = new Eleve();
+		System.out.println("e1: " + e1);
 
 	}
 
